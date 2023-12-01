951157c88a0646cac8fcd31bd2f66fc3d52cfc18
omsant02 <omsantoshwar02@gmail.com>
2023-12-02 00:39:55 +0530

    add (register) route and mongodb (only input data- register)

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -3,7 +3,7 @@ import Right from "../icons/Right";
 
 export default function Hero() {
   return (
-    <section className="hero">
+    <section className="hero mt-4">
       <div className="py-12">
         <h1 className="text-4xl font-semibold ">
           Everything
@@ -20,7 +20,7 @@ export default function Hero() {
             Order now
             <Right />
           </button>
-          <button className="flex gap-2 py-2 text-gray-600 font-semibold">
+          <button className="flex gap-2 py-2 text-gray-600 font-semibold rounded-full">
             Learn more
             <Right />
           </button>
