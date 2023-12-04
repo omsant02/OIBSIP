7f56d74cfd5edaaeef969932820c5a32ddbeee4c
omsant02 <omsantoshwar02@gmail.com>
2023-12-04 11:57:38 +0530

    add upload and saved animations using react-hot-toast(toast) instead using normal text

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -5,14 +5,13 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import InfoBox from "@/components/layout/InfoBox";
 import SuccessBox from "@/components/layout/SuccessBox";
+import toast from "react-hot-toast";
+import { resolve } from "path";
 
 export default function ProfilePage() {
   const session = useSession();
   const [userName, setUserName] = useState("");
   const [image, setImage] = useState("");
-  const [saved, setSaved] = useState(false);
-  const [isSaving, setIsSaving] = useState(false);
-  const [isUploading, setIsUploading] = useState(false);
   const { status } = session;
 
   useEffect(() => {
@@ -24,17 +23,25 @@ export default function ProfilePage() {
 
   async function handleProfileInfoUpdate(ev) {
     ev.preventDefault();
-    setSaved(false);
-    setIsSaving(true);
-    const response = await fetch("/api/profile", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name: userName, image }),
+
+    const savingPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch("/api/profile", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: userName, image }),
+      });
+      if (response.ok) {
+        resolve();
+      } else {
+        reject();
+      }
     });
-    setIsSaving(false);
-    if (response.ok) {
-      setSaved(true);
-    }
+
+    await toast.promise(savingPromise, {
+      loading: "Saving",
+      success: "Profile saved!",
+      error: "Error",
+    });
   }
 
   async function handleFileChange(ev) {
@@ -42,15 +49,24 @@ export default function ProfilePage() {
     if (files?.length === 1) {
       const data = new FormData();
       data.set("file", files[0]);
-      setIsUploading(true);
-      const response = await fetch("/api/upload", {
+
+      const uploadPromise = fetch("/api/upload", {
         method: "POST",
         body: data,
+      }).then((response) => {
+        if (response.ok) {
+          return response.json().then((link) => {
+            setImage(link);
+          });
+        }
+        throw new Error("Something went wrong");
       });
 
-      const link = await response.json();
-      setImage(link);
-      setIsUploading(false);
+      await toast.promise(uploadPromise, {
+        loading: "Uploading...",
+        success: "Upload complete",
+        error: "Upload error",
+      });
     }
   }
 
@@ -67,10 +83,6 @@ export default function ProfilePage() {
       <h1 className="text-center text-primary text-4xl mb-4">Profile</h1>
 
       <div className="max-w-md mx-auto">
-        {saved && <SuccessBox>Profile saved!</SuccessBox>}
-        {isSaving && <InfoBox>Saving...</InfoBox>}
-        {isUploading && <InfoBox>Uploading...</InfoBox>}
-
         <div className="flex gap-4 items-center">
           <div>
             <div className=" p-2 rounded-lg relative max-w-[120px]">
