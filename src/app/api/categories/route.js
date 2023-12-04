136d284af83ab6e68d5c890e4d8b7fefe97f273c
omsant02 <omsantoshwar02@gmail.com>
2023-12-05 01:38:30 +0530

    add functionality to category option (in profile)

---
+++ src/app/api/categories/route.js
@@ -0,0 +1,22 @@
+// import { isAdmin } from "@/app/api/auth/[...nextauth]/route";
+import { Category } from "@/models/Category";
+import mongoose from "mongoose";
+
+export async function POST(req) {
+  const { name } = await req.json();
+  const categoryDoc = await Category.create({ name });
+  return Response.json(categoryDoc);
+}
+
+export async function PUT(req) {
+  mongoose.connect(process.env.MONGO_URL);
+  const { _id, name } = await req.json();
+
+  await Category.updateOne({ _id }, { name });
+  return Response.json(true);
+}
+
+export async function GET() {
+  mongoose.connect(process.env.MONGO_URL);
+  return Response.json(await Category.find());
+}
--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,26 +1,110 @@
 "use client";
 import UserTabs from "@/components/layout/UserTabs";
 import { useEffect, useState } from "react";
+import { useProfile } from "../../components/UseProfile";
+import { resolve } from "path";
+import toast from "react-hot-toast";
 
 export default function CategoriesPage() {
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [categoryName, setCategoryName] = useState("");
+  const [categories, setCategories] = useState([]);
+  const { loading: ProfileLoading, data: profileData } = useProfile();
+  const [editedCategory, setEditedCategory] = useState(null);
 
   useEffect(() => {
-    fetch("/api/profile").then((response) => {
-      response.json().then((data) => {
-        setIsAdmin(data.admin);
-      });
-    });
+    fetchCategories();
   }, []);
 
-  if (!isAdmin) {
+  function fetchCategories() {
+    fetch("/api/categories").then((res) => {
+      res.json().then((categories) => {
+        setCategories(categories);
+      });
+    });
+  }
+
+  async function handleCategorySubmit(ev) {
+    ev.preventDefault();
+
+    const creationPromise = new Promise(async (resolve, reject) => {
+      const data = { name: categoryName };
+      if (editedCategory) {
+        data._id = editedCategory._id;
+      }
+      const response = await fetch("/api/categories", {
+        method: editedCategory ? "PUT" : "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      setCategoryName("");
+      fetchCategories();
+      setEditedCategory(null);
+      if (response.ok) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+    await toast.promise(creationPromise, {
+      loading: editedCategory
+        ? "Updating category..."
+        : "Creating your new category...",
+      success: editedCategory ? "Category updated" : "Category created",
+      error: "Error, sorry...",
+    });
+  }
+
+  if (ProfileLoading) {
+    return "Loading user info...";
+  }
+
+  if (!profileData.admin) {
     return "Not an admin";
   }
 
   return (
-    <section className="mt-8 max-w-lg mx-auto">
+    <section className="mt-8 max-w-md mx-auto">
       <UserTabs isAdmin={true} />
-      categories
+      <form className="mt-8" onSubmit={handleCategorySubmit}>
+        <div className="flex gap-2 items-end">
+          <div className="grow">
+            <label>
+              {editedCategory ? "Update category" : "New category name"}
+              {editedCategory && (
+                <>
+                  : <b>{editedCategory.name}</b>
+                </>
+              )}
+            </label>
+            <input
+              type="text"
+              value={categoryName}
+              onChange={(ev) => setCategoryName(ev.target.value)}
+            />
+          </div>
+          <div className="pb-2 flex gap-2">
+            <button className="border border-primary" type="submit">
+              {editedCategory ? "Update" : "Create"}
+            </button>
+          </div>
+        </div>
+      </form>
+      <div>
+        <h2 className="mt-8 text-sm text-gray-500">Existing categories</h2>
+        {categories?.length > 0 &&
+          categories.map((c) => (
+            <button
+              type="button"
+              onClick={() => {
+                setEditedCategory(c);
+                setCategoryName(c.name);
+              }}
+              className="bg-gray-200 rounded-xl p-2 px-4 flex gap-1 cursor-pointer mb-1"
+            >
+              <span> {c.name}</span>
+            </button>
+          ))}
+      </div>
     </section>
   );
 }
