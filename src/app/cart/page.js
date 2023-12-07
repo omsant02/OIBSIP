6975842206354a6df8f628d55bdd1c8e89ddb34b
omsant02 <omsantoshwar02@gmail.com>
2023-12-07 14:19:15 +0530

    refactor

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -15,6 +15,14 @@ export default function CartPage() {
   const { data: profileData } = useProfile();
 
   useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (window.location.href.includes("canceled=1")) {
+        toast.error("Payment failed 😔");
+      }
+    }
+  }, []);
+
+  useEffect(() => {
     if (profileData?.city) {
       const { phone, streetAddress, city, postalCode, country } = profileData;
       const addressFromProfile = {
@@ -35,6 +43,35 @@ export default function CartPage() {
 
   function handleAddressChange(propName, value) {
     setAddress((prevAddress) => ({ ...prevAddress, [propName]: value }));
+  }
+
+  async function proceedToCheckout(ev) {
+    ev.preventDefault();
+    // address and shopping cart products
+
+    const promise = new Promise((resolve, reject) => {
+      fetch("/api/checkout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          address,
+          cartProducts,
+        }),
+      }).then(async (response) => {
+        if (response.ok) {
+          resolve();
+          window.location = await response.json();
+        } else {
+          reject();
+        }
+      });
+    });
+
+    await toast.promise(promise, {
+      loading: "Preparing your order...",
+      success: "Redirecting to payment...",
+      error: "Something went wrong... Please try again later",
+    });
   }
 
   return (
@@ -89,14 +126,24 @@ export default function CartPage() {
                 </div>
               </div>
             ))}
-          <div className="py-2 text-right pr-16">
-            <span className="text-gray-500">Subtotal:</span>
-            <span className="text-lg font-semibold pl-2">${subtotal}</span>
+          <div className="py-2 flex justify-end items-center pr-16">
+            <div className="text-gray-500">
+              Subtotal:
+              <br />
+              Delivery:
+              <br />
+              Total:
+            </div>
+            <div className="font-semibold pl-2 text-right">
+              ${subtotal} <br />
+              $5
+              <br />${subtotal + 5}
+            </div>
           </div>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg">
           <h2>Checkout</h2>
-          <form>
+          <form onSubmit={proceedToCheckout}>
             <AddressInputs
               addressProps={address}
               setAddressProp={handleAddressChange}
