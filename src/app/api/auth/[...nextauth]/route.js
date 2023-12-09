17e0f4f977f1cdab87c71d522b2bfd4e796bcf9a
omsant02 <omsantoshwar02@gmail.com>
2023-12-10 04:00:17 +0530

    fix error in auth api

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,27 +1,14 @@
+// import clientPromise from "@/libs/mongoConnect";
+import { UserInfo } from "@/models/UserInfo";
 import bcrypt from "bcrypt";
 import * as mongoose from "mongoose";
 import { User } from "@/models/User";
-import { UserInfo } from "@/models/UserInfo";
 import NextAuth, { getServerSession } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "@/libs/mongoConnect";
+// import { MongoDBAdapter } from "@auth/mongodb-adapter";
 
-export async function isAdmin() {
-  const session = await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-  if (!userEmail) {
-    return false;
-  }
-  const userInfo = await UserInfo.findOne({ email: userEmail });
-  if (!userInfo) {
-    return false;
-  }
-  return userInfo.admin;
-}
-
-const handler = NextAuth({
+export const authOptions = {
   secret: process.env.SECRET,
   // adapter: MongoDBAdapter(clientPromise),
   providers: [
@@ -41,9 +28,10 @@ const handler = NextAuth({
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        const { email, password } = credentials;
-        mongoose.connect(process.env.MONGO_URL);
+        const email = credentials?.email;
+        const password = credentials?.password;
 
+        mongoose.connect(process.env.MONGO_URL);
         const user = await User.findOne({ email });
         const passwordOk = user && bcrypt.compareSync(password, user.password);
 
@@ -55,6 +43,21 @@ const handler = NextAuth({
       },
     }),
   ],
-});
+};
+
+export async function isAdmin() {
+  const session = await getServerSession(authOptions);
+  const userEmail = session?.user?.email;
+  if (!userEmail) {
+    return false;
+  }
+  const userInfo = await UserInfo.findOne({ email: userEmail });
+  if (!userInfo) {
+    return false;
+  }
+  return userInfo.admin;
+}
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
